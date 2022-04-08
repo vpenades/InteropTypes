4a8d0577def502d702d084c51b63e63ee74e4181
vpenades <5433822+vpenades@users.noreply.github.com>
2022-04-08 16:18:38 +0200

    mor landmarks 3d changes

--- src/InteropTypes.Vision.Core/States/Landmarks3D.cs
+++ src/InteropTypes.Vision.Core/States/Landmarks3D.cs
@@ -17,11 +17,35 @@ namespace InteropTypes.Vision
     {
         #region lifecycle
 
-        public Landmarks3D() { }
+        public Landmarks3D()
+        {
+            _OnDraw = _DrawPoints;
+        }
         
         public Landmarks3D(params int[] innerIndices)
         {
-            _LineIndices = innerIndices;
+            void drawLines(IScene3D dc, Landmarks3D lll)
+            {
+                // ideally this should be half the distance of the two closest points
+                // but that's not trivial to calculate.
+                var r = 0.25f * _GetSize().Length() / (Landmarks.Count + 1);
+
+                for (int i = 1; i < innerIndices.Length; i += 2)
+                {
+                    var a = innerIndices[i - 1];                    
+                    if (a >= lll._Landmarks.Length) continue;
+
+                    var b = innerIndices[i + 0];
+                    if (b >= lll._Landmarks.Length) continue;
+
+                    var aa = lll._Landmarks[a];
+                    var bb = lll._Landmarks[b];
+
+                    dc.DrawSegment(aa, bb, r * 2, Color.White);
+                }                
+            }
+
+            _OnDraw = drawLines;
         }
 
         #endregion
@@ -30,14 +54,10 @@ namespace InteropTypes.Vision
 
         private Score _Score;
         private XYZ[] _Landmarks;
+        private RectangleF? _Bounds;
+        private XYZ? _Size;
 
-        private RectangleF? _InnerBounds;
-        
-
-        private readonly int[] _LineIndices;
-        private RectangleF? _LineBounds;
-
-        private XYZ? _Size;        
+        private Action<IScene3D, Landmarks3D> _OnDraw;
 
         #endregion
 
@@ -49,51 +69,24 @@ namespace InteropTypes.Vision
 
         public DateTime CaptureTime { get; set; }
 
-        public IReadOnlyList<XYZ> Landmarks => _Landmarks ?? Array.Empty<XYZ>();
-
-        /// <summary>
-        /// Represents the bounds of the landmarks defined by <see cref="_LineIndices"/>.<br/>
-        /// Otherwise, it's the same as OuterBounds.
-        /// </summary>
-        public RectangleF InnerBounds
-        {
-            get
-            {
-                if (_InnerBounds.HasValue) return _InnerBounds.Value;
-
-                if (_LineIndices == null) { _InnerBounds = this.OuterBounds; }
-                else
-                {
-                    var landmarks = _LineIndices.Select(idx => _Landmarks[idx]);
-
-                    // it could be useful to have an (min,max) = AggregateTuple((a,b) => min, (a,b)=> max);
-                    var min = landmarks.Aggregate((a, b) => XYZ.Min(a, b));
-                    var max = landmarks.Aggregate((a, b) => XYZ.Max(a, b));
-                    var siz = max - min;
-
-                    _InnerBounds = new RectangleF(min.X, min.Y, siz.X, siz.Y);
-                }
-
-                return _InnerBounds.Value;
-            }
-        }
+        public IReadOnlyList<XYZ> Landmarks => _Landmarks ?? Array.Empty<XYZ>();        
 
         /// <summary>
         /// Represents the landmarks bounds in the XY Axis.
         /// </summary>
-        public RectangleF OuterBounds
+        public RectangleF Bounds
         {
             get
             {
-                if (_LineBounds.HasValue) return _LineBounds.Value;
+                if (_Bounds.HasValue) return _Bounds.Value;
 
                 var min = _Landmarks.Aggregate((a, b) => XYZ.Min(a, b));
                 var max = _Landmarks.Aggregate((a, b) => XYZ.Max(a, b));
                 var siz = max - min;
 
-                _LineBounds = new RectangleF(min.X, min.Y, siz.X, siz.Y);
+                _Bounds = new RectangleF(min.X, min.Y, siz.X, siz.Y);
 
-                return _LineBounds.Value;
+                return _Bounds.Value;
             }
         }
 
@@ -104,25 +97,22 @@ namespace InteropTypes.Vision
         public void Clear()
         {
             _Score = (0, false);
-            _LineBounds = null;
-            _InnerBounds = null;
+            _Bounds = null;
+            _Size = null;
         }
 
         public void CopyTo(ref Landmarks3D other)
         {
-            if (other == null || this._LineIndices != other._LineIndices)
-            {
-                other = new Landmarks3D(this._LineIndices);
-            }
+            if (other == null) other = new Landmarks3D();
+
+            other._OnDraw = this._OnDraw;
 
             other.Name = this.Name;
+            other.CaptureTime = this.CaptureTime;
 
             other.SetLandmarks(this.Score, this._Landmarks);
-            other._InnerBounds = this._InnerBounds;
-            other._LineBounds = this._LineBounds;
+            other._Bounds = this._Bounds;
             other._Size = this._Size;
-
-            other.CaptureTime = this.CaptureTime;            
         }            
 
         public void SetLandmarks(Score score)
@@ -181,7 +171,7 @@ namespace InteropTypes.Vision
 
         #endregion
 
-        #region drawing        
+        #region drawing
 
         private XYZ _GetSize()
         {
@@ -194,17 +184,19 @@ namespace InteropTypes.Vision
             return _Size.Value;
         }
 
-        public virtual void DrawTo(IScene3D dc)
+        public virtual void DrawTo(IScene3D dc) { _OnDraw?.Invoke(dc, this); }
+
+        private static void _DrawPoints(IScene3D dc, Landmarks3D lll)
         {
             // ideally this should be half the distance of the two closest points
             // but that's not trivial to calculate.
-            var r = 0.25f * _GetSize().Length() / (Landmarks.Count + 1);
+            var r = 0.25f * lll._GetSize().Length() / (lll.Landmarks.Count + 1);
 
-            foreach(var l in Landmarks)
+            foreach (var l in lll._Landmarks)
             {
                 dc.DrawSphere(l, r * 2f, Color.White);
-            }
-        }        
+            }            
+        }
 
         #endregion
     }
