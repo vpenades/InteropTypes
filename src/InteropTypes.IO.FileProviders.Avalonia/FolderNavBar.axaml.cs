8a035a284e7a801e50695b46898f87ed2db88036
vpenades <5433822+vpenades@users.noreply.github.com>
2023-08-02 11:11:06 +0200

    avalonia: improving folder navigation

---
+++ src/InteropTypes.IO.FileProviders.Avalonia/FolderNavBar.axaml.cs
@@ -0,0 +1,65 @@
+using System.IO;
+
+using Avalonia;
+using Avalonia.Controls;
+
+using DIRECTORYINFO = InteropTypes.IO.PhysicalDirectoryInfo;
+
+namespace InteropTypes.IO
+{
+    public partial class FolderNavBar : UserControl
+    {
+        public FolderNavBar()
+        {
+            InitializeComponent();
+
+            this.Loaded += FolderNavBar_Loaded;
+        }
+
+        private void FolderNavBar_Loaded(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            CurrentDirectory = DIRECTORYINFO.ApplicationDirectory;
+        }
+
+        public static readonly DirectProperty<FolderNavBar, DIRECTORYINFO> CurrentDirectoryProperty
+            = AvaloniaProperty.RegisterDirect<FolderNavBar, DIRECTORYINFO>(nameof(CurrentDirectory), o => o.CurrentDirectory, (o, v) => o.CurrentDirectory = v);
+
+        private DIRECTORYINFO _CurrentDir;
+
+        public DIRECTORYINFO CurrentDirectory
+        {
+            get { return _CurrentDir; }
+            set
+            {
+                if (SetAndRaise(CurrentDirectoryProperty, ref _CurrentDir, value))
+                {
+                    myCurrentPath.Text = _CurrentDir.PhysicalPath;
+                }
+            }
+        }
+
+        public void _OnClick_FolderUp(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            var pdinfo = CurrentDirectory;
+            
+            var dinfo = new System.IO.DirectoryInfo(pdinfo.PhysicalPath).Parent;
+            if (dinfo == null) return;
+
+            CurrentDirectory = new DIRECTORYINFO(dinfo);
+            
+        }
+
+        public void _OnClick_GoToAppDir(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            CurrentDirectory = DIRECTORYINFO.ApplicationDirectory;
+        }
+
+        public void _OnClick_FolderBrowse(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            if (DIRECTORYINFO.TryBrowseFolderDialog(out var dinfo, null, null, null))
+            {
+                CurrentDirectory = dinfo;
+            }
+        }
+    }
+}
