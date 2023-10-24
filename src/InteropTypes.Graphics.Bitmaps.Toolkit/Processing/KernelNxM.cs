091a71f41a59eea2102318f5e01477feff880502
vpenades <5433822+vpenades@users.noreply.github.com>
2023-10-24 16:09:13 +0200

    Added KernelNxM

---
+++ src/InteropTypes.Graphics.Bitmaps.Toolkit/Processing/KernelNxM.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using InteropTypes.Diagnostics;
+
+namespace InteropTypes.Graphics.Bitmaps
+{
+    namespace Processing
+    {
+        [System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Sequential)]
+        public readonly struct KernelNxM<TPixel>
+            where TPixel : unmanaged
+        {
+            #region constructor
+
+            private static KernelNxM<TPixel> CreateRadial(int w, int h, Func<float,TPixel> weightToPixel)
+            {
+                var k = new Processing.KernelNxM<TPixel>(w, h);
+                float rx = k.Width / 2;
+                float ry = k.Height / 2;
+
+                float _getRadius(int x, int y)
+                {
+                    var xx = (x - rx) / rx;
+                    var yy = (y - ry) / ry;
+                    return 1 - Math.Clamp(MathF.Sqrt(xx * xx + yy * yy), 0, 1);
+                }
+
+                float www = 0;
+
+                for (int y = 0; y < k.Height; ++y)
+                {
+                    for (int x = 0; x < k.Width; ++x)
+                    {
+                        www += _getRadius(x, y);
+                    }
+                }
+
+                for (int y = 0; y < k.Height; ++y)
+                {
+                    for (int x = 0; x < k.Width; ++x)
+                    {
+                        var r = _getRadius(x, y) / www;
+                        k[x, y] = weightToPixel(r);
+                    }
+                }
+
+                return k;
+            }
+
+            internal KernelNxM(int w, int h)
+            {
+                _Rows = new TPixel[w * h];
+                Width = w;
+                Height = h;
+            }
+
+            #endregion
+
+            #region data            
+
+            internal readonly TPixel[] _Rows;
+
+            public readonly int Width;
+            public readonly int Height;
+
+            public TPixel this[int x, int y]
+            {
+                get => _Rows[y * Width + x];
+                set => _Rows[y * Width + x] = value;
+            }
+
+            #endregion
+
+            #region API
+
+            private void Update(in _RollingRows rows, ReadOnlySpan<int> xindices)
+            {
+                for(int y=0; y < Height; y++)
+                {
+                    for(int x=0; x < Width; x++)
+                    {
+                        this[x,y] = rows[y][xindices[x]];
+                    }
+                }
+            }
+
+            public static void Apply(SpanBitmap<TPixel> dstBitmap, (int n, int m) ksize, KernelEvaluator<TPixel> function)
+            {
+                var rows = new _RollingRows(dstBitmap.Width, ksize.m);
+                for(int i=0; i < rows.Height; i++)
+                {
+                    dstBitmap.GetScanlinePixels(0).CopyTo(rows[i]);
+                }                
+
+                var indices = new int[ksize.n];
+                var kernel = new KernelNxM<TPixel>(ksize.n, ksize.m);
+
+                for (int y = 0; y < dstBitmap.Height; y++)
+                {
+                    // cycle tmp rows:
+                    rows.Roll();
+                    dstBitmap.GetScanlinePixels(Math.Min(dstBitmap.Height - 1, y + 1)).CopyTo(rows.LastRow);
+
+                    // apply to dst row:
+
+                    var dstRow = dstBitmap.UseScanlinePixels(y);
+
+                    for (int x = 0; x < dstBitmap.Width; x++)
+                    {
+                        for(int i=0; i < indices.Length; i++)
+                        {                            
+                            indices[i] = Math.Clamp(x + i - ksize.n/2, 0, rows.Width-1);
+                        }
+
+                        kernel.Update(rows, indices);
+                        dstRow[x] = function(kernel);
+                    }                    
+                }
+            }
+
+            #endregion
+
+            #region nested types
+
+            public delegate TResult KernelEvaluator<TResult>(in KernelNxM<TPixel> kernel);
+
+            internal struct _RollingRows
+            {
+                public _RollingRows(int width, int rows)
+                {
+                    _Rows = new TPixel[width * rows];
+                    Width = width;
+                    Height = rows;
+                    _Scroll = 0;
+                }
+
+                private readonly TPixel[] _Rows;
+
+                public readonly int Width;
+                public readonly int Height;
+
+                private int _Scroll;
+
+                public readonly Span<TPixel> this[int index]
+                {
+                    get
+                    {
+                        index += _Scroll;
+                        index %= Height;
+                        return _Rows.AsSpan(Width * index, Width);
+                    }
+                }
+
+                public Span<TPixel> LastRow => this[Height - 1];
+
+                public void Roll()
+                {
+                    ++_Scroll;
+                }
+            }
+
+            #endregion
+        }
+    }
+}
--- tests/InteropTypes.Graphics.Bitmaps.Tests/Transform/Kernet3x3Tests.cs
+++ tests/InteropTypes.Graphics.Bitmaps.Tests/Transform/Kernet3x3Tests.cs
@@ -36,10 +36,89 @@ namespace InteropTypes.Graphics.Bitmaps.Transform
             v -= kernel.P32.RGBA;
             v -= kernel.P33.RGBA;
             v = System.Numerics.Vector4.Min(System.Numerics.Vector4.One, v);
-            v = System.Numerics.Vector4.Max(System.Numerics.Vector4.Zero, v);            
+            v = System.Numerics.Vector4.Max(System.Numerics.Vector4.Zero, v);
 
             return new Pixel.BGR24(new Pixel.RGBA128F(v));
         }
+
+
+
+        [TestCase("Resources\\shannon.webp", 15)]
+        [TestCase("Resources\\cat.png", 5)]
+        public void Blur1(string resName, int ksize)
+        {
+            var filePath = System.IO.Path.Combine(TestContext.CurrentContext.TestDirectory, resName);
+            
+            var bmp = MemoryBitmap<Pixel.BGRA32>.Load(filePath, Codecs.SkiaCodec.Default);
+
+
+            Processing.KernelNxM<Pixel.BGRA32>.Apply(bmp, (ksize, ksize), BoxGlow);
+
+            bmp.Save(new AttachmentInfo("result.png"));
+        }
+
+
+        private static TPixel BoxGlow<TPixel>(in Processing.KernelNxM<TPixel> kernel)
+            where TPixel : unmanaged
+        {
+            var top = default(Pixel.BGRP32);
+            var bot = default(Pixel.BGRP32);
+
+            top.Set(kernel[kernel.Width / 2, kernel.Height / 2]); // central
+            bot.Set(BoxBlur(kernel));            
+
+            bot.SetSourceOver(top, 256);
+
+            return bot.To<TPixel>();
+        }
+
+
+        private static TPixel BoxBlur<TPixel>(in Processing.KernelNxM<TPixel> kernel)
+            where TPixel : unmanaged
+        {
+            var xyzw = System.Numerics.Vector4.Zero;
+            var rgbp = default(Pixel.RGBP32);
+
+            for (int y = 0; y < kernel.Height; ++y)
+            {
+                for (int x = 0; x < kernel.Width; ++x)
+                {
+                    rgbp.Set(kernel[x, y]);
+                    xyzw += new System.Numerics.Vector4(rgbp.PreR,rgbp.PreG, rgbp.PreB, rgbp.A) / 255f;
+                }
+            }
+
+            xyzw /= kernel.Width * kernel.Height;
+
+            xyzw = System.Numerics.Vector4.Min(System.Numerics.Vector4.One, xyzw);
+            xyzw = System.Numerics.Vector4.Max(System.Numerics.Vector4.Zero, xyzw);            
+
+            return new Pixel.RGBP128F(xyzw).To<TPixel>();
+        }
+
+
+        private static Pixel.BGRA32 Blur(in Processing.KernelNxM<Pixel.RGBP128F> kernel, Processing.KernelNxM<Pixel.Luminance32F> weights)
+        {
+            var v = System.Numerics.Vector4.Zero;            
+
+            for (int y = 0; y < kernel.Height; ++y)
+            {
+                for (int x = 0; x < kernel.Width; ++x)
+                {
+                    var p = kernel[x, y];
+                    var w = weights[x, y];
+
+                    v += p.RGBP * w.L;                    
+                }
+            }
+            
+            v = System.Numerics.Vector4.Min(System.Numerics.Vector4.One, v);
+            v = System.Numerics.Vector4.Max(System.Numerics.Vector4.Zero, v);
+
+            return new Pixel.BGRA32(new Pixel.RGBP128F(v));
+        }
+
+        
 
     }
 }
