673c6feb91034e61cc039481605bfbf7eab7143a
Vicente Penades <5433822+vpenades@users.noreply.github.com>
2023-12-10 13:40:36 +0100

    Added CryptoStream

---
+++ src/InteropTypes.Crypto/SymmetricFactory.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Linq;
+
+namespace InteropTypes.Crypto
+{
+    /// <summary>
+    /// Represents the base class for symmetric algorythm factories
+    /// </summary>
+    internal abstract class SymmetricFactory
+    {
+        public static Byte[] GetRandomBytes(int count)
+        {
+            using (var rnd = System.Security.Cryptography.RandomNumberGenerator.Create())
+            {
+                var data = new byte[count];
+                rnd.GetBytes(data);
+                return data;
+            }
+        }
+
+        public abstract SymmetricAlgorithm CreateAlgorythm();
+    }
+
+    sealed class AESFactory : SymmetricFactory
+    {
+        #region lifecycle
+
+        public const int SALTSIZE = 16;
+
+        public static AESFactory FromRandom()
+        {
+            using (var aes = Aes.Create())
+            {
+                // https://security.stackexchange.com/questions/52665/which-is-the-best-cipher-mode-and-padding-mode-for-aes-encryption
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+                aes.GenerateKey();
+                aes.GenerateIV();
+
+                var key = new AESFactory();
+
+                key._Key = aes.Key;
+                key._IV = aes.IV;
+                key._Mode = aes.Mode;
+                key._Padding = aes.Padding;
+
+                return key;
+            }
+        }
+
+        public static AESFactory FromPassword(string clearPassword, byte[] salt = null)
+        {
+            var passwordBytes = System.Text.Encoding.UTF8.GetBytes(clearPassword);
+
+            return _Create(passwordBytes, salt);
+        }
+
+        private static AESFactory _Create(byte[] passwordBytes, byte[] salt = null)
+        {
+            var (aesKey, aesIV) = salt == null
+                ? GetPasswordSHA256(passwordBytes)
+                : GetPasswordRfc2898(passwordBytes, salt);
+
+            if (aesKey.Length != 32) throw new InvalidOperationException();
+            if (aesIV.Length != 16) throw new InvalidOperationException();
+
+            var aes = new AESFactory();
+
+            aes._Key = aesKey;
+            aes._IV = aesIV;
+            aes._Mode = CipherMode.CBC; // https://security.stackexchange.com/questions/52665/which-is-the-best-cipher-mode-and-padding-mode-for-aes-encryption
+            aes._Padding = PaddingMode.PKCS7;
+
+            return aes;
+        }
+
+        // https://stackoverflow.com/questions/70913958/best-way-to-store-passwords
+
+        private static (Byte[] key, Byte[] iv) GetPasswordSHA256(byte[] passwordBytes)
+        {
+            using var sha256 = SHA256.Create();
+            using var md5 = MD5.Create();
+
+            var key = sha256.ComputeHash(passwordBytes);
+            var iv = md5.ComputeHash(passwordBytes);
+
+            return (key, iv);
+        }
+
+        private static (Byte[] key, Byte[] iv) GetPasswordRfc2898(byte[] passwordBytes, byte[] salt)
+        {
+            if (salt.Length != 16) throw new ArgumentOutOfRangeException(nameof(salt));
+
+            // https://stackoverflow.com/questions/67574726/generating-aes-iv-from-rfc2898derivebytes
+            // https://crypto.stackexchange.com/questions/26537/derive-cipher-iv-and-cipher-key-using-pbkdf2-with-random-salt
+            // https://csharp.hotexamples.com/es/examples/System.Security.Cryptography/Rfc2898DeriveBytes/-/php-rfc2898derivebytes-class-examples.html
+
+            using (var rfc = new Rfc2898DeriveBytes(passwordBytes, salt, 641487, HashAlgorithmName.SHA256))
+            {
+                var key = rfc.GetBytes(32);
+                rfc.GetBytes(6);
+                var iv = rfc.GetBytes(16);
+                return (key, iv);
+            }
+        }
+
+        #endregion
+
+        #region data
+
+        private CipherMode _Mode;
+        private PaddingMode _Padding;
+        private Byte[] _Key;
+        private Byte[] _IV;
+
+        #endregion
+
+        #region API
+
+        public override SymmetricAlgorithm CreateAlgorythm()
+        {
+            var aes = Aes.Create();
+            aes.Key = _Key;
+            aes.IV = _IV;
+            aes.Mode = _Mode;
+            aes.Padding = _Padding;
+            return aes;
+        }        
+
+        #endregion        
+
+    }    
+}
---
+++ src/InteropTypes.Crypto/_CryptoStream.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace InteropTypes.Crypto
+{
+    class _CryptoStream : CryptoStream
+    {
+        #region lifecycle
+
+        public static _CryptoStream CreateFrom(Stream plainStream, CryptoStreamMode streamMode, SymmetricAlgorithm algo)
+        {
+            var xform = streamMode == CryptoStreamMode.Read
+                ? algo.CreateDecryptor()
+                : algo.CreateEncryptor();
+
+            return new _CryptoStream(plainStream, null, xform, streamMode, true);
+        }
+
+        public static _CryptoStream CreateFromAES(Stream plainStream, CryptoStreamMode streamMode, string password, bool useSalt)
+        {
+            if (!useSalt)
+            {
+                var factory = AESFactory.FromPassword(password);
+
+                return CreateFromFactory(plainStream, streamMode, factory);
+            }
+            else
+            {
+                byte[] salt;
+
+                switch (streamMode)
+                {
+                    case CryptoStreamMode.Read:
+                        salt = new Byte[AESFactory.SALTSIZE];
+                        if (TryReadBytesToEnd(plainStream, salt) != salt.Length) throw new EndOfStreamException();
+                        break;
+                    case CryptoStreamMode.Write:
+                        salt = SymmetricFactory.GetRandomBytes(AESFactory.SALTSIZE);
+                        plainStream.Write(salt, 0, salt.Length);
+                        break;
+                    default: throw new NotSupportedException(streamMode.ToString());
+                }
+
+                var factory = AESFactory.FromPassword(password, salt);
+
+                return CreateFromFactory(plainStream, streamMode, factory);
+            }
+        }
+
+        public static _CryptoStream CreateFromFactory(Stream plainStream, CryptoStreamMode streamMode, SymmetricFactory factory)
+        {
+            var aes = factory.CreateAlgorythm();
+
+            var xform = streamMode == CryptoStreamMode.Read
+                ? aes.CreateDecryptor()
+                : aes.CreateEncryptor();
+
+            return new _CryptoStream(plainStream, aes, xform, streamMode, true);
+        }
+
+        protected _CryptoStream(Stream stream, SymmetricAlgorithm algo, ICryptoTransform transform, CryptoStreamMode mode, bool leaveOpen)
+            : base(stream, transform, mode, leaveOpen)
+        {
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            if (disposing)
+            {
+                System.Threading.Interlocked.Exchange(ref _Transform, null)?.Dispose();
+                System.Threading.Interlocked.Exchange(ref _Algorythm, null)?.Dispose();
+            }
+        }
+
+        #endregion
+
+        #region data
+
+        private ICryptoTransform _Transform;
+        private SymmetricAlgorithm _Algorythm;
+
+        #endregion
+
+        #region API
+
+        private static int TryReadBytesToEnd(Stream stream, ArraySegment<Byte> bytes)
+        {
+
+
+            var bbb = bytes;
+
+            while (bbb.Count > 0)
+            {
+                var l = stream.Read(bbb.Array, bbb.Offset, bbb.Count);
+                if (l <= 0) return bytes.Count - bbb.Count;
+
+                bbb = bbb.Slice(l);
+            }
+
+            return bytes.Count;
+        }
+
+        #endregion
+    }
+}
