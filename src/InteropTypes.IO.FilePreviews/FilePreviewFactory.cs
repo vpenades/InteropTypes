684f31aafac6fc3d743798c5169c2127162cea8d
Vicente Penades Armengot <5433822+vpenades@users.noreply.github.com>
2025-02-25 17:55:54 +0100

    Added FilePreview Service

---
+++ src/InteropTypes.IO.FilePreviews/FilePreviewFactory.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Threading.Tasks;
+
+using InteropTypes.Graphics;
+
+namespace InteropTypes.IO
+{
+    public static class FilePreviewFactory
+    {
+        [return: NotNull]
+        public static WindowsBitmap GetPreviewOrDefault(System.IO.FileInfo finfo)
+        {
+            if (finfo == null || !finfo.Exists) return null;
+
+            // if (!_FilterExtension(finfo.Extension)) return null;
+
+            if (OperatingSystem.IsWindowsVersionAtLeast(6, 0, 6000))
+            {
+                return Platforms.Win32.FilePreview_Win32_COM.GetPreviewOrDefault(finfo);
+            }
+
+            return null;
+        }
+
+        [return: NotNull]
+        public static async Task<WindowsBitmap> GetPreviewOrDefaultAsync(System.IO.FileInfo finfo)
+        {
+            if (finfo == null || !finfo.Exists) return null;
+
+            // if (!_FilterExtension(finfo.Extension)) return null;
+
+            if (OperatingSystem.IsWindowsVersionAtLeast(6, 0, 6000))
+            {
+                return await Task.Run(() => Platforms.Win32.FilePreview_Win32_COM.GetPreviewOrDefault(finfo)).ConfigureAwait(true);
+            }
+
+            return null;
+        }
+
+        private static bool _FilterExtension(string ext)
+        {
+            ext = ext.ToUpperInvariant();
+
+            // positives
+            if (ext.EndsWith(".JPG")) return true;
+            if (ext.EndsWith(".PNG")) return true;
+            if (ext.EndsWith(".GIF")) return true;
+            if (ext.EndsWith(".BMP")) return true;
+            if (ext.EndsWith(".WEBP")) return true;
+
+            // negatives
+            if (ext.EndsWith(".MD")) return false;
+            if (ext.EndsWith(".TXT")) return false;
+            if (ext.EndsWith(".PDF")) return false;
+            if (ext.EndsWith(".DOC")) return false;
+            if (ext.EndsWith(".DOCX")) return false;
+
+            if (ext.EndsWith(".DLL")) return false;
+            if (ext.EndsWith(".EXE")) return false;
+            if (ext.EndsWith(".JSON")) return false;
+
+            return true;
+        }
+    }
+}
---
+++ src/InteropTypes.IO.FilePreviews/Platforms/Win32/Win32.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows.Win32
+{    
+    internal partial class PInvoke
+    {
+        // https://learn.microsoft.com/es-es/windows/win32/api/shobjidl_core/nf-shobjidl_core-ishellitemimagefactory-getimage
+
+        [SupportedOSPlatform("windows5.1.2600")]
+        internal static unsafe nuint SHGetFileInfo(string pszPath, Storage.FileSystem.FILE_FLAGS_AND_ATTRIBUTES dwFileAttributes, ref UI.Shell.SHFILEINFOW sfi, UI.Shell.SHGFI_FLAGS uFlags)
+        {
+            fixed (UI.Shell.SHFILEINFOW* psfi = &sfi)
+            fixed (char* pszPathLocal = pszPath)
+            {
+                return SHGetFileInfo(pszPathLocal, dwFileAttributes, psfi, (uint)Unsafe.SizeOf<UI.Shell.SHFILEINFOW>(), uFlags);
+            }
+        }
+
+        [SupportedOSPlatform("windows6.0.6000")]
+        public static Windows.Win32.UI.Shell.IShellItemImageFactory GetShellItemImageFactory(string filePath)
+        {
+            var guid = new Guid("BCC18B79-BA16-442F-80C4-8A59C30C463B"); // this is the GUID of IShellItemImageFactory
+
+            Windows.Win32.System.Com.IBindCtx bindContext = null;
+
+            if (Windows.Win32.PInvoke.SHCreateItemFromParsingName(filePath, bindContext, guid, out var context).Failed) return null;
+
+            return context as Windows.Win32.UI.Shell.IShellItemImageFactory;
+        }
+
+        internal static unsafe DeleteObjectSafeHandle GetImage(this UI.Shell.IShellItemImageFactory factory, Foundation.SIZE size, UI.Shell.SIIGBF flags)
+        {
+            // COMException 0x8004B200 < expected
+
+            factory.GetImage(size, flags, out var safeHandle);
+            if (safeHandle.IsInvalid)
+            {
+                safeHandle.Dispose();
+                return null;
+            }
+
+            return safeHandle;
+        }
+
+        [SupportedOSPlatform("windows6.0.6000")]
+        internal static unsafe Graphics.Gdi.HBITMAP GetThumbnail(this UI.Shell.IThumbnailProvider provider, uint cx, out UI.Shell.WTS_ALPHATYPE alpha)
+        {
+            var hbmp = default(Windows.Win32.Graphics.Gdi.HBITMAP);
+            Graphics.Gdi.HBITMAP* pbmp = &hbmp;
+
+            fixed (UI.Shell.WTS_ALPHATYPE* palpha = &alpha)
+            {
+                provider.GetThumbnail(cx, pbmp, palpha);
+            }
+
+            return hbmp;
+        }
+    }
+}
