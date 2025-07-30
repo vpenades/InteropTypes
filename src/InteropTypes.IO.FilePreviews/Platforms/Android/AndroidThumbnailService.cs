0a8e74ae151591bacecb3b70e839946a7b62e3ef
Vicente Penades Armengot <5433822+vpenades@users.noreply.github.com>
2025-07-30 18:09:59 +0200

    Added android and IOS preview thumbnail code (untested)

---
+++ src/InteropTypes.IO.FilePreviews/Platforms/Android/AndroidThumbnailService.cs
@@ -0,0 +1,45 @@
+﻿using Android.Content;
+using Android.Media;
+using Android.OS;
+using Android.Provider;
+
+
+
+using System.IO;
+using System.Threading.Tasks;
+
+namespace InteropTypes.Platforms.Android
+{
+    internal static class AndroidThumbnailService
+    {
+        public static async Task<System.IO.Stream?> GetStreamThumbnailAsync(string filePath, int width, int height)
+        {            
+            using var thumb = await GetAndroidThumbnailAsync(filePath, width, height);
+
+            var ms = new MemoryStream();
+            thumb.Compress(Android.Graphics.Bitmap.CompressFormat.Png, 100, ms);
+            ms.Position = 0;
+            return ms;
+        }
+
+        public static async Task<Android.Graphics.Bitmap?> GetAndroidThumbnailAsync(string filePath, int width, int height)
+        {
+            var context = Android.App.Application.Context;
+            var uri = Android.Net.Uri.Parse(filePath);
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Q)
+            {
+                // Android 10+: ContentResolver.LoadThumbnailAsync
+                return = await context.ContentResolver
+                    .LoadThumbnailAsync(uri, new Android.Util.Size(width, height), null)
+                    .AsAsync<Android.Graphics.Bitmap>();
+            }
+            else
+            {
+                // Fallback for older releases
+                long id = ContentUris.ParseId(uri);
+                return MediaStore.Images.Thumbnails.GetThumbnail(context.ContentResolver, id, ThumbnailKind.MiniKind, null);
+            }
+        }
+    }
+}
---
+++ src/InteropTypes.IO.FilePreviews/Platforms/IOS/IOSThumbnailService.cs
@@ -0,0 +1,47 @@
+﻿using Foundation;
+
+using Microsoft.Maui.Controls;
+
+using QuickLookThumbnailing;
+
+using System.IO;
+using System.Threading.Tasks;
+
+using UIKit;
+
+namespace InteropTypes.Platforms.IOS
+{
+    internal class IOSThumbnailService
+    {
+        public static async partial Task<System.IO.Stream?> GetStreamThumbnailAsync(string filePath, int width, int height)
+        {
+            var fileUrl = NSUrl.FromFilename(filePath);
+
+            var request = new QLThumbnailGeneratorRequest(
+                fileUrl,
+                new CoreGraphics.CGSize(width, height),
+                QLThumbnailGeneratorRequestRepresentationTypes.Icon);
+
+            var sem = new System.Threading.SemaphoreSlim(0, 1);
+            QLThumbnailRepresentation? repr = null;
+
+            QLThumbnailGenerator.SharedGenerator.GenerateBestRepresentation(
+                request,
+                (thumbnail, error) =>
+                {
+                    repr = thumbnail;
+                    sem.Release();
+                });
+
+            await sem.WaitAsync();
+
+            if (repr?.Image is UIImage img)
+            {
+                var data = img.AsPNG();
+                return new MemoryStream(data.ToArray());
+            }
+
+            return null;
+        }
+    }
+}
